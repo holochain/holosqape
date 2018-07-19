fd8ec287d4597113e46795a97e638fa2ed7d4186
Nicolas Luck <nicolas@lucksus.eu>
2018-07-19 20:45:08 +0200

    Adjust example test to work on vanilla HoloSqape installation.

--- app-test-scaffold/test.js
+++ app-test-scaffold/test.js
@@ -1,10 +1,9 @@
 var test = require('tape');
 
 test('timing test', function (t) {
-    t.plan(5);
+    t.plan(4);
     t.equal(3, 3);
-    t.equal(Container.installedApps().length, 3);
+    t.equal(Container.installedApps().length, 2);
     t.equal(Container.installedApps()[0], "A3F21RqpuAse0U8UqodHW8yi/Sh7elz5FJ4sAcevChg=");
-    t.equal(Container.installedApps()[1], "ZJ4w6xur5xqwWSZlcaQf7mfr+tBbf3owiC1XKWkDgLo=");
-    t.equal(Container.installedApps()[2], "uTS2nMUDg0xs+o9QQtZz0QoIFFdfv8teQse82qMsXCg=");
+    t.equal(Container.installedApps()[1], "uTS2nMUDg0xs+o9QQtZz0QoIFFdfv8teQse82qMsXCg=");
 });
